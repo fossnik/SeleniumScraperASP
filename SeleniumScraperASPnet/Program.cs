b892eb080eb4f2beddf9c7bee25735d4f1d09652
Zack <zackhartmann@gmail.com>
2018-04-09 19:05:39 -0400

    Implemented Saving Data with Entity

--- SeleniumScraperASPnet/Program.cs
+++ SeleniumScraperASPnet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using SeleniumScraperASPnet.Model;
 
 namespace SeleniumScraperASPnet
@@ -10,9 +11,11 @@ namespace SeleniumScraperASPnet
         {
             // Linux Environment:
             // please note that it is necessary to set the environment varible "TERM" to "xterm"
+
+            List<Coin> snapshot;
             try
             {
-                List<Coin> snapshot = Selenium.Scraper.CompileSnapshot();
+                snapshot = Selenium.Scraper.CompileSnapshot();
 
                 Console.WriteLine("Scraped " + snapshot.Count + " coins\n");
                 foreach (var coinObject in snapshot)
@@ -27,6 +30,19 @@ namespace SeleniumScraperASPnet
                 throw;
             }
 
+            using (var db = new SnapshotContext())
+            {
+                // build dbContext object from the list of coin objects procured via selenium
+                var marketSnapshot = new MarketSnapshot
+                {
+                    Coins = snapshot,
+                    SnapTime = DateTime.Now
+                };
+
+                // append to database
+                db.MarketSnapshots.Add(marketSnapshot);
+                db.SaveChanges();
+            }
 
             Environment.Exit(0);
         }
