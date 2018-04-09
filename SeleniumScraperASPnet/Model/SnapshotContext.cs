524601b05aa9152b0496f6a37212afc0fb0aecf3
Zack <zackhartmann@gmail.com>
2018-04-09 15:38:25 -0400

    Implemented DbSet Derived Context Class

---
+++ SeleniumScraperASPnet/Model/SnapshotContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumScraperASPnet.Model
+{
+    class SnapshotContext : DbContext
+    {
+        public DbSet<MarketSnapshot> MarketSnapshots { get; set; }
+        public DbSet<Coin> Coins { get; set; }
+    }
+}
