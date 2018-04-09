2fa602f04ff5f46dd3caffb0d6bb94914cef0a33
Zack <zackhartmann@gmail.com>
2018-04-09 16:22:10 -0400

    Made SnapId a Key value

--- SeleniumScraperASPnet/Model/MarketSnapshot.cs
+++ SeleniumScraperASPnet/Model/MarketSnapshot.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SeleniumScraperASPnet.Model
 {
     public class MarketSnapshot
     {
+        [Key]
         public int SnapId { get; set; }
         public DateTime SnapTime { get; set; }
 
