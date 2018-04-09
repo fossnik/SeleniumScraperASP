34f6a0665b7601ab61ceb74bee1862a55000f3b3
Zack <zackhartmann@gmail.com>
2018-04-09 18:49:22 -0400

    Created SnapshotContext Class

---
+++ SeleniumScraperASPnet/Model/SnapshotContext.cs
@@ -0,0 +1,8 @@
+﻿namespace SeleniumScraperASPnet.Model
+{
+    class SnapshotContext : DbContext
+    {
+        public DbSet<MarketSnapshot> MarketSnapshots { get; set; }
+        public DbSet<Coin> Coins { get; set; }
+    }
+}
