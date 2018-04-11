15a593a225fd4f75db377a42e7e9eb9d4d15b4c4
Zack <zackhartmann@gmail.com>
2018-04-11 12:30:42 -0400

    Created Homecontroller

---
+++ MVC-Frontend/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MVC_Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
